0769b5d2e3ae134d32fc35cdae8f74ed37dcc124
erikcas <erikcas1972@gmail.com>
2015-10-23 16:30:55 +0200

    build kernel inline and always use actual kernelheaders
    Change-Id: I63e3aaae8a6c023a79869185fca123730147babc

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-PRODUCT_VENDOR_KERNEL_HEADERS := device/sony/yukon/kernel-headers
+TARGET_KERNEL_SOURCE =: kernel/sony/msm
 
 TARGET_ARCH := arm
 TARGET_ARCH_VARIANT := armv7-a-neon
@@ -114,4 +114,3 @@ ifeq ($(HOST_OS),linux)
 endif
 
 BUILD_KERNEL := true
--include vendor/sony/kernel/KernelConfig.mk
